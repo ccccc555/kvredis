7cfb0c5f538d868328ec9b1bb41513f6e9ad0947
ccccc555 <134172516+ccccc555@users.noreply.github.com>
2023-05-21 01:39:04 +0200

    Update route.js

--- app/getkeys/route.js
+++ app/getkeys/route.js
@@ -2,7 +2,7 @@ import { kv } from "@vercel/kv";
 import { NextResponse } from 'next/server';
 
 export async function GET() {
- var keys=await kv.keys('*')
- var len=keys.length
- return NextResponse.json({ ks: keys, le: len});
+ var kys=await kv.keys('*')
+ var len=kys.length
+ return NextResponse.json({ ks: kys, le: len});
 }
