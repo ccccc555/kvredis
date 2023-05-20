ac62f023ed5d067287bbdef8c58e2c7861b545e2
ccccc555 <134172516+ccccc555@users.noreply.github.com>
2023-05-21 01:42:39 +0200

    Update route.js

--- app/getnum/route.js
+++ app/getnum/route.js
@@ -2,7 +2,7 @@ import { kv } from "@vercel/kv";
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  var ab= await kv.inc('abc')
+  var ab= await kv.incr('abc')
   var num=await kv.incr('views')
   return NextResponse.json({ nmber: num, ac: ab});
 }
