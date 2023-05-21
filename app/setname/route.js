29c32a13b24af6bb2a303ef49fb4b4fa73663b91
ccccc555 <134172516+ccccc555@users.noreply.github.com>
2023-05-21 11:59:16 +0200

    Update route.js

--- app/setname/route.js
+++ app/setname/route.js
@@ -1,7 +1,7 @@
 import { kv } from "@vercel/kv";
 import { NextResponse } from 'next/server';
 
-export async function POST() {
-  const a=await kv.set(body,'hello');
+export default async function handler(req,res) {
+  const a=await kv.set(req.body,'hello');
   return NextResponse.json({ done: 0});
 }
