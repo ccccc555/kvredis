c44c7026ed9c81013fd94e6b3a73e50168118524
ccccc555 <134172516+ccccc555@users.noreply.github.com>
2023-05-21 11:10:45 +0200

    Update route.js

--- app/setname/route.js
+++ app/setname/route.js
@@ -3,5 +3,5 @@ import { NextResponse } from 'next/server';
 
 export default async function handler(req,res) {
   const a=await kv.set('new','hello');
-  res.status(200).json({ done: 0});
+  return NextResonse.json({ done: 0});
 }
