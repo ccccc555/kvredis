28ccf6ee16aa90224857b1ad76e67a4758296180
ccccc555 <134172516+ccccc555@users.noreply.github.com>
2023-05-21 13:30:03 +0200

    Update route.js

--- app/setname/route.js
+++ app/setname/route.js
@@ -1,7 +1,7 @@
 import { kv } from "@vercel/kv";
 import { NextResponse } from 'next/server';
 
-export async function POST(){
+export async function POST(request){
   const data=await request.json();
   
   const a=await kv.set('cam','hello');
