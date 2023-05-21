31427d8de27951e55d87cb1c86b808901a69e30a
ccccc555 <134172516+ccccc555@users.noreply.github.com>
2023-05-21 13:15:01 +0200

    Update route.js

--- app/setname/route.js
+++ app/setname/route.js
@@ -4,6 +4,6 @@ import { NextResponse } from 'next/server';
 export async function POST(){
   const data=await request.json();
   
-  const a=await kv.set(data.word,'hello');
+  const a=await kv.set(data.body.word,'hello');
   return NextResponse.json({ done: 0});
 }
